1db1890c1af0aea8cb3f53e553b7875edc843bef
sogolm <sogol.moshtaghi@gmail.com>
2015-06-18 22:23:40 -0700

    mvp

--- tutorial/tutorial/pipelines.py
+++ tutorial/tutorial/pipelines.py
@@ -8,10 +8,14 @@ import json
 
 class JsonWriterPipeline(object):
 
-	def __init__(self):
-		self.file = open('items.jl', 'wb')
+    def __init__(self):
+        self.file = open('items.jl', 'wb')
 
-	def process_item(self, item, spider):
-		line = json.dumps(dict(item)) + "\n"
-		self.file.write(line)
-		return item
+    def process_item(self, item, spider):
+        line = json.dumps(dict(item)) + "\n"
+        self.file.write(line)
+        return item
+
+
+    #TODO: HANDLE DUPLICATE IDS?
+
--- tutorial/tutorial/spiders/yp_spider.py
+++ tutorial/tutorial/spiders/yp_spider.py
@@ -2,8 +2,12 @@ import scrapy
 from tutorial.items import YpItem
 import urlparse
 from scrapy.http import Request
+from scrapy.contrib.spiders import CrawlSpider, Rule
+from scrapy.contrib.linkextractors import LinkExtractor
+import json
 
-class DmozSpider(scrapy.Spider):
+
+class YpSpider(CrawlSpider):
     name = "yp"
     allowed_domains = ["yellowpages.com"]
     download_delay = 2
@@ -13,36 +17,35 @@ class DmozSpider(scrapy.Spider):
     # start_urls = [
     # "http://www.yellowpages.com/search?search_terms=cupcakes&geo_location_terms=Tucson%2C+AZ"
     # ]
-    start_urls = ["http://www.yellowpages.com/tucson-az/cupcake-stores"]
+    start_urls = ["http://www.yellowpages.com/tucson-az/cupcake-stores?page="]
+    rules = [Rule(LinkExtractor(allow=['\d+']), 'parse_search')]
 
     #use Link extractor from CrawlSpider to move between pages.
 
-    def parse(self, response):
-        #base_url =  response.url.split("/search")[-2]
+    def parse_search(self, response):
+        #TODO: FIX THIS LINK TO BE UNHACKY
         base_url = "http://" + response.url.split("/")[-3] 
         requests = []
         for business in response.xpath('//h3[@class="n"]'):
             url = business.xpath('a/@href')[0].extract()
-            if url[0] != "/": # if it doesn't start with '/' ignore -- external link
+            if url[0] != "/": # if it doesn't start with '/' ignore -- external link #TODO: IS THERE A BETTER WAY TO HANDLE THIS?
                 continue
             else:
                 url = urlparse.urljoin(base_url, url)
                 requests.append(Request(url=url, callback=self.parse_business))
         return requests
 
-
     def parse_business(self, response):
         self.log("downloading business info")
         item = YpItem()
-
-        # id: how do we get the id? from base url maybe like above?
-        # response.xpath('//div[@id="mip-impression"][@data-analytics]')[0].extract().split(',')
-
-        # this returns [u'Mon - Sat', u'7:00 am - 6:00 pm', u'Sun', u'8:00 am - 2:00 pm'] -- what to do
+        data_model = response.xpath('//a[@href="#addBusiness"]/@data-model').extract()[0]
+        data_model_dict = json.loads(data_model)
+        item['name'] = data_model_dict['name']
+        item['ypid'] = data_model_dict['ypid']
         item['hours'] = response.xpath('//dd[@class="open-hours"]//div[@class="open-details"]//p//time[@datetime]//text()').extract() 
         item['general_info'] = " ".join(info for info in response.xpath('//dd[@class="description"]//text()').extract())
-        item['services'] = response.xpath('//dd//ul/li//text()').extract() # list for this?
-        item['payment_method'] = response.xpath('//dd[@class="payment"]//text()').extract() # list for this?
+        item['services'] = response.xpath('//dd//ul/li//text()').extract()
+        item['payment_method'] = response.xpath('//dd[@class="payment"]//text()').extract()
         item['location'] = " ".join(loc for loc in response.xpath('//dd[@class="location-description"]//text()').extract())
         item['aka'] = response.xpath('//dd[@class="aka"]//p/text()').extract()
         item['other_link'] = response.xpath('//dd[@class="weblinks"]//a[@href]//text()').extract()
@@ -57,10 +60,3 @@ class DmozSpider(scrapy.Spider):
             raise scrapy.exceptions.CloseSpider('stopping for testing')
         self.log("done downloading %d items" %self.counter)
         return item
-
-
-
-
-
-
-
